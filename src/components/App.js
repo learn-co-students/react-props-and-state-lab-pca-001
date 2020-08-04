9d1dafb2e5c4548ee9d7d05a61ad12c92239d3cf
Jo Fischer <josephinefischer9@gmail.com>
2020-08-04 14:45:33 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,33 @@ class App extends React.Component {
     }
   }
 
+  handleChange = (event) => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: event.target.value
+      }
+    })
+  }
+
+  handleFindPets = event => {
+    if (this.state.filters.type === 'all') {
+      fetch(`/api/pets`)
+      .then(response => response.json())
+      .then(pets => this.setState({pets: pets}))
+    }
+    else {
+      fetch(`/api/pets?type=${this.state.filters.type}`)
+      .then(response => response.json())
+      .then(pets => this.setState({pets: pets}))
+    }
+  }
+
+  handleAdopt = (id) => {
+    const selectedPet = this.state.pets.find(pet => pet.id == id);
+    selectedPet.isAdopted = true;
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +51,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.handleChange} onFindPetsClick={this.handleFindPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.handleAdopt}/>
             </div>
           </div>
         </div>
